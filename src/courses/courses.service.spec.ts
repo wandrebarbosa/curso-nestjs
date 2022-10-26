20825998a71eb5f246f7105d5a70c601b46b0272
wandrebarbosa <wandresonbarbosa@gmail.com>
2022-10-26 16:45:30 -0300

    implements jest test in project

---
+++ src/courses/courses.service.spec.ts
@@ -0,0 +1,52 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Connection, Repository } from "typeorm";
+import { CoursesService } from "./courses.service"
+import { Course } from "./entities/courses.entity";
+import { Tag } from "./entities/tag.entity";
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>; //definindo um jest mock para usar todos os metodos do meu serviço de forma mocada.
+
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+    findOne: jest.fn(),
+})
+
+describe('CourseService', () => {
+    let service: CoursesService;
+    let courseRepository: MockRepository;
+
+    beforeEach( async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                CoursesService,
+                { provide: Connection, useValue: {}},
+                { provide: getRepositoryToken(Course), useValue: createMockRepository()},
+                { provide: getRepositoryToken(Tag), useValue: createMockRepository()},
+            ],
+        }).compile();
+
+        service = module.get<CoursesService>(CoursesService);
+        courseRepository = module.get<MockRepository>(getRepositoryToken(Course))
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined()
+    });
+
+    describe('findOne', () => {
+        describe('buscar curso pelo ID', () => {
+            it('deve retorna um objeto Course', async () => {
+                const courseId = '1';
+                const expectedCourse = {}
+
+                courseRepository.findOne.mockReturnValue(expectedCourse)
+                const course = await service.findOne(courseId);
+                expect(course).toEqual(expectedCourse);
+            });
+
+            it('deve retornar um NotFoundException', () => {
+
+            });
+        })
+    });
+});
---
+++ src/examples/calc.spec.ts
@@ -0,0 +1,10 @@
+export function add(x: number, y: number) {
+    return x + y;
+}
+
+describe('Initial test', () => {
+    it('add function', () => {
+        expect(add(1, 2)).toEqual(3);
+    });
+});
+
